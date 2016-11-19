9bef369a2453549b0fa89228dd1702fa6cbe341e
Горбачев Алексей <gorbachev.info@gmail.com>
2016-11-20 02:52:38 +0300

    Module User work

--- mvc/config/autoload/global.php
+++ mvc/config/autoload/global.php
@@ -11,6 +11,40 @@
  * file.
  */
 
+use Zend\Session\Storage\SessionArrayStorage;
+use Zend\Session\Validator\RemoteAddr;
+use Zend\Session\Validator\HttpUserAgent;
+
 return [
-    // ...
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ],
+    ],
+    'db' => [
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=confuciy_sosiski;host=db35.valuehost.ru',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ],
+    ],
+    // Session configuration.
+    'session_config' => [
+        // Session cookie will expire in 1 hour.
+        'cookie_lifetime' => 60*60*1,
+        // Session data will be stored on server maximum for 30 days.
+        'gc_maxlifetime'     => 60*60*24*30,
+    ],
+    // Session manager configuration.
+    'session_manager' => [
+        // Session validators (used for security).
+        'validators' => [
+            RemoteAddr::class,
+            HttpUserAgent::class,
+        ]
+    ],
+    // Session storage configuration.
+    'session_storage' => [
+        'type' => SessionArrayStorage::class
+    ],
 ];
